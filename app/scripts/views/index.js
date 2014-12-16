47a6eb8e110d41f7dc420956c9e432af53ae9a03
linder0209 <linder0209@126.com>
2014-12-16 23:50:51 +0800

    add flexslider

--- app/scripts/views/index.js
+++ app/scripts/views/index.js
@@ -1,11 +1,13 @@
 (function (view) {
+  'use strict';
   view.index = {};
 
-  $.extend(view.index,{
-    init: function(){
+  $.extend(view.index, {
+    init: function () {
       this.initEvent();
+      this.flexSlider();
     },
-    initEvent: function(){
+    initEvent: function () {
       $('#login').click(function (e) {
         e.preventDefault();
         e.stopPropagation();
@@ -22,15 +24,51 @@
         $('#loginPanel').hide();
       });
 
-      $(document.body).click(function(e){
+      $(document).click(function (e) {
         var el = $(e.target);
-        if(el.closest('#aboutMenu,#loginPanel').length > 0){
+        if (el.closest('#aboutMenu,#loginPanel').length > 0) {
           return;
         }
         $('#aboutMenu,#loginPanel').hide();
         $('#about').find('i').removeClass().addClass('white-downArrow');
       });
+
+      $('#menuList').on('mouseenter', '>li', function (e) {
+        e.stopPropagation();
+        var $el = $(e.target);
+        var $parentEl = $el.closest('ul');
+        //reset
+        $parentEl.find('li>div').hide();
+        $parentEl.find('li>a>i').removeClass().addClass('gray-downArrow');
+        $el.find('>div').show();
+        $el.find('>a>i').addClass('gray-upArrow');
+      }).on('mouseleave',function(){
+        var $el = $(e.target);
+        $el.find('>div').hide();
+        $el.find('>a>i').removeClass().addClass('gray-downArrow');
+      });
+    },
+    flexSlider:function(){
+      // The slider being synced must be initialized first
+      $('#carousel').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        asNavFor: '#slider'
+      });
+
+      $('#slider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel"
+      });
     }
+
   });
 
-})(ZP_View);
+})(hopeView);
